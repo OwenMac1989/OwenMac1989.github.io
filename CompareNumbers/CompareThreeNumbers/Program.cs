d894dc80b4e2c204251a031bfaa28834c4051b17
OwenMac1989 <130425428+OwenMac1989@users.noreply.github.com>
2023-04-28 05:43:08 -0600

    Add files via upload

---
+++ CompareNumbers/CompareThreeNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿// Tyler Creager Janauary 25 2023
+// CompareThreeNumbers
+using System;
+using System.Diagnostics.Metrics;
+using static System.Console;
+class CompareThreeNumbers
+{
+    static void Main()
+    {
+        string numberString;
+        int num1, num2, num3;
+        Write("Enter an integer");
+        numberString = ReadLine();
+        num1 = Convert.ToInt32(numberString);
+        Write("Enter an integer");
+        numberString = ReadLine();
+        num2 = Convert.ToInt32(numberString);
+        Write("Enter an interger");
+        numberString = ReadLine();
+        num3 = Convert.ToInt32(numberString);
+        if (num1 == num2)
+            if (num1 == num3)
+                WriteLine("All three numbers are equal");
+            else
+                WriteLine("First two are equal");
+        else
+            if (num1 == num3)
+            WriteLine("First and Last are equal");
+        else
+            if (num2 == num3)
+            WriteLine("Last two are equal");
+        else
+            WriteLine("No two Numbers are Equal");
+
+
+    }
+}
